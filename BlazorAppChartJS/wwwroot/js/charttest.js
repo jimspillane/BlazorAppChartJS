54e9c04b7d648c7caab2c63661863ca2af0d8a9b
Jim Spillane <james.a.spillane@gmail.com>
2020-10-05 18:27:01 -0400

    Clean up

--- BlazorAppChartJS/wwwroot/js/charttest.js
+++ BlazorAppChartJS/wwwroot/js/charttest.js
@@ -1,32 +1,32 @@
-﻿import { Chart, BarController, Rectangle, LineController, Line, Point, LinearScale, CategoryScale, Title, Tooltip, Filler, Legend } from
-    'https://cdn.jsdelivr.net/npm/chart.js@3.0.0-beta.3/dist/chart.esm.js';
+﻿import { Chart, BarController, Rectangle, LinearScale, CategoryScale } from
+    "https://cdn.jsdelivr.net/npm/chart.js@3.0.0-beta.3/dist/chart.esm.js";
 
 export async function MakeChart(element) {
 
-    Chart.register(BarController, Rectangle, LineController, Line, Point, LinearScale, CategoryScale, Title, Tooltip, Filler, Legend);
+    Chart.register(BarController, Rectangle, LinearScale, CategoryScale);
 
-    var myChart = new Chart(element, {
-        type: 'bar',
+    const myChart = new Chart(element, {
+        type: "bar",
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
             datasets: [{
-                label: '# of Votes',
+                label: "# of Votes",
                 data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)"
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)"
                 ],
                 borderWidth: 1
             }]
